b774a8f3f55b0b0f7e7aa015079d341f48ad3e39
7013145 <tolle_95@hotmail.com>
2018-10-05 18:11:12 +0200

    onefox class

--- HttpMD5Util.py
+++ HttpMD5Util.py
@@ -10,12 +10,9 @@ sleep_time = 3*3 + 1
 sleep_max = 60 * 5
 
 
-def httpGet(url, resource, params, source):
+def httpGet(url, resource, params):
     sleep_count = 0
     sleep_alarm = False
-    if source == "btc_usd":
-        source = "@OKCoinBTC"
-
     while True:
 
         try:
@@ -28,15 +25,15 @@ def httpGet(url, resource, params, source):
             sleep_count += (sleep_time + 1)
             logging.info("Sleeping in httpGet, " + str(sleep_count) + " seconds")
             if sleep_alarm == False and sleep_count >= sleep_max:
-                string = source + " has been down for over " + str(sleep_max/60) + " minutes"
+                string = "resource has been down for over " + str(sleep_max/60) + " minutes"
                 logging.info(string)
                 sleep_alarm = True
             continue
         else:
             if sleep_alarm == True:
-                string = source + " is now back up after being down since " + start_time
+                string = "resource is now back up after being down since " + start_time
                 logging.info(string)
                 sleep_count = 0
                 sleep_alarm = False
             break
-        return data
+    return data
